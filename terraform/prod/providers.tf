bb8d7370c8a241d44bb275a17a097add4ce87554
BenJoyenConseil <b.joyenconseil@gmail.com>
2022-03-24 21:28:53 +0100

    etape4 : déplacer et mutualiser tf

--- terraform/prod/providers.tf
+++ terraform/prod/providers.tf
@@ -1,5 +1,4 @@
 provider "aws" {
   region  = "eu-west-3"
   profile = "duck"
-  version = "4.6.0"
 }
--- terraform/prod/version.tf
+++ terraform/prod/version.tf
@@ -1,3 +1,6 @@
 terraform {
   required_version = "1.1.6"
+  required_providers {
+    aws = "4.6.0"
+  }
 }
